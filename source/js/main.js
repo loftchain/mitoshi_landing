72356b94902b2f4547e64c001bc149c93e3f478b
mickbetch <serezha11shumov@mail.ru>
2018-09-12 18:15:57 +0300

    add dynamic effects to header while scroll page

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 $(document).ready(function() {
+  var header = $('.header');
   var popup = $('.header__toggle');
   var menu = $('.header__group');
 
@@ -7,4 +8,14 @@ $(document).ready(function() {
     popup.toggleClass('header__toggle--active');
     menu.toggleClass('header__group--active');
   });
+
+  window.onscroll = function() {
+    var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrolled > 0 && !header.hasClass('header--white')) {
+      header.addClass('header--white');
+    } else if (scrolled === 0 && header.hasClass('header--white')) {
+      header.removeClass('header--white');
+    }
+  }
 });
